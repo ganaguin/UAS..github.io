38ce58b92528589c1b7f6b308d41c37178a39406
ibnu khairul <47640130+ibnukhairul@users.noreply.github.com>
2024-07-18 17:01:28 +0700

    Update script.js

--- base/script.js
+++ base/script.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Smooth scrolling for navigation links
     document.querySelectorAll('nav ul li a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            const href = this.getAttribute('href');
+            if (href.startsWith('#')) {
+                e.preventDefault();
+                document.querySelector(href).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 
